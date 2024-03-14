ab23d0ec1aecae62d74dc7bd5091d31e6b301532
Ed Cuevas <edjanncuevas@icloud.com>
2024-03-14 11:56:32 +0900

    feat: remove nextjs defaults

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="w-full h-auto min-h-screen">
       
     </main>
   );
