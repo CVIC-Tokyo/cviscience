f308556e5d47cd65a661bc437c500091b7188e8f
Ed Cuevas <edjanncuevas@icloud.com>
2024-03-14 12:35:53 +0900

    feat: initialize home dropdown component

---
+++ src/app/components/navcomponents/HomeDropdown.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomeDropdown: React.FC<HomeDropdownProps> = () => {
+
+    return (
+        <div className="">
+
+        </div>
+    )
+}
+
+export default HomeDropdown
+;
