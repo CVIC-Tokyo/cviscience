36441491fc7003d02e4932ee9c15826405acb514
Ed Cuevas <edjanncuevas@icloud.com>
2024-03-14 16:01:31 +0900

    feat: implement footer component

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,14 +1,31 @@
+'use client'
+
 import React from "react";
-import cvic_logo_box_red from "../../../public/logos/cvic-logo-box-red.png"
+import cvic_logo_600 from "../../../public/logos/cvic_logo_600.png"
 import Image from "next/image";
+import { useGlobalContext } from "../Context/store";
+import { getLocaleData } from "@/utils/helpers";
 
 const Footer: React.FC<FooterProps> = () => {
+  const { locale } = useGlobalContext();
+  const localeData = getLocaleData(locale);
+
   return (
     <div className="bg-[#820000] text-white w-full h-auto p-3 flex flex-col justify-center items-center align-bottom">
       <div className="flex flex-col justify-center items-center">
-        <Image src={cvic_logo_box_red} width={100} height={100} alt="/CVIC logo" />
-        {}
+        <Image src={cvic_logo_600} width={500} height={100} alt="/CVIC logo" />
+        <p className="footer-text">
+          {localeData.CVIC_INFO.CLINIC_DESCRIPTION}
+        </p>
+        <p className="footer-text">
+          {localeData.CVIC_INFO.CLINIC_ADDRESS}
+        </p>
+        <p className="footer-text">
+          {localeData.CVIC_INFO.CLINIC_RIGHTS_RESERVED}
+        </p>
       </div>
     </div>
   )
 }
+
+export default Footer;
