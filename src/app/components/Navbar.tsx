372faf77385e1971d2978aa9065ea6f11aeae6fc
Ed Cuevas <edjanncuevas@icloud.com>
2024-03-14 11:56:53 +0900

    feat: add logo to navigation bar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,9 @@
 'use client'
 
 import React, { useEffect, useState } from "react";
+import Link from "next/link"
+import Image from "next/image";
+import cvic_logo_600 from "@/../public/logos/cvic_logo_600.png"
 
 const Navbar: React.FC<NavbarProps> = () => {
     const [showSidebar, setShowSidebar] = useState<boolean>(false);
@@ -36,6 +39,11 @@ const Navbar: React.FC<NavbarProps> = () => {
         <div
             className={`fixed top-0 left-0 right-0 z-[100] ${showShadow ? `shadow-lg bg-opacity-75` : ''}`}
         >
+            <div className="max-w-[1240px] mx-auto flex justify-between items-center p-2">
+                <Link href={"/"} className="scroll-false">
+                    <Image src={cvic_logo_600} alt="CVIC logo" width={600} unoptimized/>
+                </Link>
+            </div>
         </div>
     )
 
