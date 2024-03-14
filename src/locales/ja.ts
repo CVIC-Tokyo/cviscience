43d83009757f70d98ce4b3fb6e9fb44c671c74c4
Ed Cuevas <edjanncuevas@icloud.com>
2024-03-14 12:42:04 +0900

    feat: initialize helper and locale data

---
+++ src/locales/ja.ts
@@ -0,0 +1,29 @@
+const ja = {
+    BASIC: {
+        HOME: 'ホーム',
+        ABOUT: 'CVICについて',
+        DOCTORS: '医師',
+        EQUIPMENTS: '機器',
+        SERVICES: 'サービス',
+        PROCESS: 'プロセス',
+        ACCESS: 'アクセス',
+        MORE: '詳細',
+    },
+    CONTACT: {
+        GET_IN_TOUCH: 'お問い合わせ',
+        NAME: '名前',
+        SURNAME: '姓',
+        EMAIL: 'Eメール',
+        SUBJECT: '件名',
+        MESSAGE: 'メッセージ',
+        SUBMIT: '送信',
+        AGREE_TO_PRIVACY_POLICY: 'プライバシーポリシーに同意する'
+    },
+    CVIC_INFO: {
+        CLINIC_NAME: '心臓画像クリニック',
+        CLINIC_ADDRESS: '東京都新宿区新小川町1-14 飯田橋リープレックスビズ 4F',
+        CLINIC_DESCRIPTION: '心臓MRI検査、心臓ドック、画像診断の専門家チーム',
+    },
+};
+
+export default ja;
---
+++ src/utils/helpers.ts
@@ -0,0 +1,13 @@
+import en from "@/locales/en";
+import ja from "@/locales/ja";
+
+export const getLocaleData = (locale: string) => {
+    switch (locale) {
+        case 'en':
+            return en;
+        case 'ja':
+            return ja;
+        default:
+            return en;
+    }
+};
