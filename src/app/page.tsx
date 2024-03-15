269497fed70a04ceb32f36f39277b84da3310468
Ed Cuevas <edjanncuevas@icloud.com>
2024-03-15 13:32:36 +0900

    feat: add created components to page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,18 @@
 
 import React from "react";
 import About from "./components/About";
+import Equipments from "./components/Equipments";
+import Doctors from "./components/Doctors";
+import Services from "./components/Services";
 
 export default function Home() {
   
   return(
   <main className="w-full h-auto pt-36 md:pt-38 lg:pt-48 min-h-screen">
     <About />
+    <Services />
+    <Equipments />
+    <Doctors />
   </main>
   )
 }
