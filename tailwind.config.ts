47dc0afbfb150132bcc7bceaf4212d2395c6c034
Ed Cuevas <edjanncuevas@icloud.com>
2024-03-19 09:53:39 +0900

    feat: add images to tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,9 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'parallax': 'url("/images/access_image.jpg")',
+        'services': 'url("/images/services_image.JPG")',
+        'equipments': 'url("/images/vintage_titan.jpg")',
       },
     },
   },
