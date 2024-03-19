5a1019108c9ea104c264d5bf6aecac4c94118856
Ed Cuevas <edjanncuevas@icloud.com>
2024-03-19 12:17:11 +0900

    fix: remove unused useEffect

--- src/app/components/tiles/Access.tsx
+++ src/app/components/tiles/Access.tsx
@@ -1,39 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useGlobalContext } from "../../Context/store";
 import { getLocaleData } from "@/utils/helpers";
-import { BiRightArrowAlt } from "react-icons/bi";
-import Image from "next/image";
 
 const Access: React.FC<AccessProps> = () => {
-  const [zoomOut, setZoomOut] = useState(false);
   const { locale } = useGlobalContext();
   const localeData = getLocaleData(locale);
   const containerRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          setTimeout(() => {
-            setZoomOut(true);
-          }, 0);
-        } else {
-          setZoomOut(false); // Zoom out when not on screen
-        }
-      },
-      { threshold: 0.5 }, // Adjust threshold as needed
-    );
-
-    if (containerRef.current) {
-      observer.observe(containerRef.current);
-    }
-
-    return () => {
-      if (containerRef.current) {
-        observer.unobserve(containerRef.current);
-      }
-    };
-  }, []);
 
   return (
     <div
