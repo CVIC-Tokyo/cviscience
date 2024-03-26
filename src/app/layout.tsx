226920b25aa4d30ab83e167149fa148f78fae430
Ed Cuevas <edjanncuevas@icloud.com>
2024-03-27 08:46:14 +0900

    feat: implement nextui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { GlobalContextProvider } from "../context/store";
 import Access from "./components/Access";
+import { NextUIProvider } from "@nextui-org/react";
 
 export const metadata: Metadata = {
   title: "Cardiovascular Imaging Clinic",
@@ -20,14 +21,16 @@ export default function RootLayout({
         async
         src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBIC_MAPS_API_KEY}&loading=async&callback=initMap`}
       ></script>
-      <body className="noto-serif w-full h-full pt-16 md:pt-40">
-        <GlobalContextProvider>
-          <Navbar />
-          {children}
-          <Access />
-          <Footer />
-        </GlobalContextProvider>
-      </body>
+      <NextUIProvider>
+        <body className="noto-serif w-full h-full pt-16 md:pt-40">
+          <GlobalContextProvider>
+            <Navbar />
+            {children}
+            <Access />
+            <Footer />
+          </GlobalContextProvider>
+        </body>
+      </NextUIProvider>
     </html>
   );
 }
