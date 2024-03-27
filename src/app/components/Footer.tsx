066ca796d789a2e42aae7b7948e133ef0721e973
Ed Cuevas <edjanncuevas@icloud.com>
2024-03-27 15:12:17 +0900

    feat: change footer logo to box

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import cvic_logo_600 from "../../../public/logos/cvic_logo_600.png";
+import cvic_logo_600 from "../../../public/logos/cvic_logo_box.jpg";
 import Image from "next/image";
 import { useGlobalContext } from "../../context/store";
 import { getLocaleData } from "@/utils/helpers";
@@ -14,13 +14,13 @@ const Footer: React.FC<FooterProps> = () => {
   return (
     <div className="bg-cvic-dark text-white w-full h-auto p-3 flex justify-center items-center align-bottom">
       <div className="flex flex-col justify-center items-center w-full h-full">
-        <Image src={cvic_logo_600} width={500} height={100} alt="/CVIC logo" />
-        <Contact />
+        <Image src={cvic_logo_600} width={75} height={75} alt="/CVIC logo" />
         <p className="footer-text">{localeData.CVIC_INFO.CLINIC_DESCRIPTION}</p>
         <p className="footer-text">{localeData.CVIC_INFO.CLINIC_ADDRESS}</p>
         <p className="footer-text">
           {localeData.CVIC_INFO.CLINIC_RIGHTS_RESERVED}
         </p>
+        <Contact />
       </div>
     </div>
   );
