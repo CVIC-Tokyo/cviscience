c2ed140a5afb32459c57ad3afea0d21b17f2fc52
Ed Cuevas <edjanncuevas@icloud.com>
2024-03-27 15:03:27 +0900

    style: respond to hover for contact buttons

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -2,8 +2,10 @@ import { getLocaleData } from "@/utils/helpers";
 import React from "react";
 import { useGlobalContext } from "../../context/store";
 import { PiPhoneCallFill } from "react-icons/pi";
-import { FaArrowRightLong } from "react-icons/fa6";
-import { LinkIcon } from "@nextui-org/react";
+import { FaExternalLinkAlt } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
+import { MdMailOutline } from "react-icons/md";
+import { IoMailSharp } from "react-icons/io5";
 
 const Contact: React.FC<ContactProps> = () => {
   const { locale } = useGlobalContext();
@@ -13,7 +15,7 @@ const Contact: React.FC<ContactProps> = () => {
     <div className="w-full h-auto flex justify-center items-center">
       <div className="grid md:grid-cols-2 w-auto">
         <div className="col-span-1 flex items-center justify-center p-2 text:xs md:text-xl text-black">
-          <div className="w-full h-full tracking-wide bg-white hover:shadow-lg font-bold p-2 flex flex-col items-center cursor-pointer">
+          <div className="w-full h-full tracking-wide bg-white hover:bg-white/75 font-bold p-2 flex flex-col items-center cursor-pointer">
             <p>
               {localeData.REACH.NUMBER}
             </p>
@@ -24,9 +26,9 @@ const Contact: React.FC<ContactProps> = () => {
           </div>
         </div>
         <div className="col-span-1 flex items-center justify-center p-2 text:xs md:text-xl text-black">
-        <div className="w-full h-full tracking-wide bg-white hover:shadow-lg font-bold p-4 flex flex-col items-center cursor-pointer">
+        <div className="w-full h-full tracking-wide bg-white hover:bg-white/75 font-bold p-4 flex flex-col items-center cursor-pointer">
             {localeData.REACH.APPLY_ONLINE}
-            <LinkIcon />
+            <IoMailSharp />
           </div>
         </div>
       </div>
