257be2c19da824bc06acf05ad8e60be5140eeac3
Ed Cuevas <edjanncuevas@icloud.com>
2024-03-29 10:50:08 +0900

    feat: add en and ja locale data for pages

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -39,15 +39,77 @@ const en = {
     MISSION: "At CVIC (Cardiac Imaging Clinic Iidabashi), our mission is simple yet profound: to transform cardiac healthcare in Japan. We are dedicated to pioneering advanced cardiac imaging diagnostics that enable the early detection and effective treatment of heart diseases. Through our relentless pursuit of excellence in technology and expertise, we aim to close the gap between Japan and the global forefront of cardiac imaging capabilities, ultimately saving lives by facilitating timely interventions.",
     VISION_TITLE: "Our Vision",
     VISION: "We envision CVIC as the undisputed leader in cardiac imaging diagnosis throughout Japan. Our vision is not merely to set new standards but to redefine them, shaping the future of cardiac healthcare with innovation and unwavering commitment. By continuously refining our techniques and fostering collaboration among healthcare professionals, we aspire to ensure that every patient receives accurate and timely diagnosis, leading to improved outcomes and enhanced quality of life. Together, we are driving forward the advancement of medical care in Japan, one heartbeat at a time.",
+    CSR_TITLE: "Corporate Social Responsibility (CSR)",
+    CSR: {
+      ONE_TITLE: "Ethical Patient Care",
+      ONE: "At CVIC, our foremost commitment is to ethical and warm patient care. We uphold the highest standards of medical ethics, ensuring patient confidentiality, transparency in procedures and costs, and the delivaery of compassionate and respectful care for individuals.",
+      TWO_TITLE: "Accessibility",
+      TWO: "We believe that every individual should have acess to quality cardiac imaging diagnostics, regardless of their financial circumstances. That's why we are committed to making our services accessbile and affordable to all members of society",
+      THREE_TITLE: "Preventive Care Advocacy",
+      THREE: "A core aspect of our CSR mission is to inform people that the leading cause of death in Japan, cardiovascular disease, is largely preventable through early detection and preventativve measures. By advocating for regular cardiac examination and promoting a proactive approach to heart health, we aim to reduce the incidence of heart disease and improve overall health outcomes in our community.",
+    }
+  },
+  SERVICES: {
+    MRI_TITLE: "Cardiac MRI",
+    MRI: "",
+    TITLE: "Time Required for Examination and Diagnosis Results",
+    PROCESS: {
+      INTRO: "Due to its specialization in cardiac examinations, CVIC boasts high expertise and achieves speedy image processing.",
+      CT_TITLE: "Cardiac CT",
+      CT_BULLET: {
+        1: "Examination Reservation: Within 3 days",
+        2: "Examination Time: 15 minutes",
+        3: "Diagnosis Results: Same day (30 minutes to 1 hour after the end of the examination)",
+      },
+      MRI_TITLE: "Cardiac MRI",
+      MRI_BULLET: {
+        1: "Examination Reservation: Within 1 to 2 weeks",
+        2: "Examination Time: 40 to 60 minutes",
+        3: "Diagnosis Results: Same day (30 minutes to 1 hour after the end of the examination)",
+      },
+      POINT: "In many medical facilities:",
+      POINT_CT: {
+        1: "Examination Reservation: 1 month wait",
+        2: "Examination Time: 15 minutes",
+        3: "Diagnosis Results: 2 to 3 days",
+      },
+      POINT_MRI: {
+        1: "Most medical facilities do not offer MRI examinations",
+        2: "Examination reservation: 2 to 3 months wait",
+        3: "Examination Time: 1 to 1.5 hours",
+        4: "Diagnosis Results: 1 week",
+      },
+    },
+  },
+  ACCESS: {
+    ACCESS_TITLE: "Access Map",
   },
 };
 
 export default en;
 
-// Our Mission
 
-// At CVIC (Cardiac Imaging Clinic Iidabashi), our mission is simple yet profound: to transform cardiac healthcare in Japan. We are dedicated to pioneering advanced cardiac imaging diagnostics that enable the early detection and effective treatment of heart diseases. Through our relentless pursuit of excellence in technology and expertise, we aim to close the gap between Japan and the global forefront of cardiac imaging capabilities, ultimately saving lives by facilitating timely interventions.
+// Introduction to CVIC
 
-// Our Vision
+// Background and Need:
 
-// We envision CVIC as the undisputed leader in cardiac imaging diagnosis throughout Japan. Our vision is not merely to set new standards but to redefine them, shaping the future of cardiac healthcare with innovation and unwavering commitment. By continuously refining our techniques and fostering collaboration among healthcare professionals, we aspire to ensure that every patient receives accurate and timely diagnosis, leading to improved outcomes and enhanced quality of life. Together, we are driving forward the advancement of medical care in Japan, one heartbeat at a time.
+// Heart disease ranks as the second leading cause of death in Japan, following cancer. The demand for early detection and treatment is steadily increasing. Collaborating with radiologists and cardiologists, CVIC strives to extend its benefits to as many patients as possible. The objective is to enhance medical care in Japan through widespread adoption of cardiac imaging diagnosis and cutting-edge technological advancements.
+// What is CVIC?
+
+// CVIC, short for Cardiac Imaging Clinic Iidabashi, commenced operations in November 2009 as a specialized imaging diagnostic center utilizing advanced medical equipment like MRI and CT. It stands as Japan's pioneering facility, focusing on circulatory system testing, particularly heart-related diagnostics. In 2014, CVIC transitioned into a standalone medical corporation, emphasizing testing and treatment independence.
+// Early Detection through Visual Examination:
+
+// The human heart's continuous motion poses a challenge to clear imaging. However, recent advancements in diagnostic imaging technologies have enabled 3D visualization and video imaging of the heart, facilitating early detection of heart diseases.
+// Challenges in Adoption of Advanced Cardiac Imaging in Japan:
+
+// Despite the benefits, widespread adoption of cardiac imaging diagnosis, notably MRI, remains limited in Japan due to its complexity and the scarcity of proficient technicians. Consequently, many individuals succumb to sudden heart attacks without prior knowledge of their heart conditions.
+// Inspiration and Mission of CVIC:
+
+// Dr. Masahiro Terashima, inspired by disparities in cardiac imaging technologies between Japan and the US, envisioned delivering the latest and finest cardiac imaging diagnostics to Japanese patients. Thus, CVIC emerged as Japan's premier specialized cardiac imaging center, aiming to bridge this technological gap.
+// Addressing Clinical Needs and Enhancing Technology:
+
+// Unlike university and general hospitals' imaging departments, CVIC prioritizes cardiac testing, responding promptly to urgent cases. Its technical prowess continually evolves through handling challenging cases, resulting in clear and high-definition cardiac images, garnering acclaim from referring physicians.
+// Expansion and Advancements within CVIC Group:
+
+// CVIC Group's expansion includes the establishment of the CVIC Medical Corporation Group's Cardiac Imaging Clinic Iidabashi, equipped with state-of-the-art testing equipment. The group's medical department, Iidabashi Heart Clinic, offers detailed consultations and accurate image screenings in a spacious environment.
+// By addressing the introduction to CVIC in this structured flow, readers gain a comprehensive understanding of its origins, objectives, technological advancements, and contributions to cardiac healthcare in Japan.
