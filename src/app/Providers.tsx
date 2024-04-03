44a794246e692bfc2aa5ee38d2d59596d11c915f
Ed Cuevas <edjanncuevas@icloud.com>
2024-04-03 17:37:11 +0900

    feat: implement preloader component in landing page

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -2,24 +2,41 @@
 
 import { GlobalContextProvider } from "@/context/store";
 import { NextUIProvider } from "@nextui-org/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/navcomponents/Navbar";
 import Access from "./components/Access";
 import Footer from "./components/Footer";
+import Preloader from "./components/Preloader";
 
 export default function Providers({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [preload, setPreload] = useState(false);
+
+  useEffect(() => {
+    setPreload(true);
+
+    setTimeout(() => {
+      setPreload(false);
+    }, 3500);
+  }, [])
+
   return (
     <NextUIProvider>
       <GlobalContextProvider>
         <div className="w-full overflow-hidden">
-          <Navbar />
-          {children}
-          <Access />
-          <Footer />
+          {
+            preload ? <Preloader />
+            :
+            <React.Fragment>
+              <Navbar />
+              {children}
+              <Access />
+              <Footer />
+            </React.Fragment>
+          }
         </div>
       </GlobalContextProvider>
     </NextUIProvider>
