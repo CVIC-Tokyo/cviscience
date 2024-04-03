bf00f02815090d8a19f4f0f498fc5a3e5ce30ab3
Ed Cuevas <edjanncuevas@icloud.com>
2024-04-03 17:36:12 +0900

    style: set preloader at the center of the component

--- src/app/components/Preloader.tsx
+++ src/app/components/Preloader.tsx
@@ -5,8 +5,8 @@ import Lottie from 'lottie-react';
 const Preloader = () => {
 
   return (
-    <div className='absolute w-full h-full bg-white'>
-      <div className='w-[300px] h-aut border-2'>
+    <div className='absolute w-full h-full bg-white flex items-center justify-center'>
+      <div className='w-[300px] h-auto'>
         <Lottie loop animationData={loading_heartbeat} />
       </div>
     </div>
