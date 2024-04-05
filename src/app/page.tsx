e3fe8cdf49f2f72864e96be94c1f51b093a1b8fb
Ed Cuevas <edjanncuevas@icloud.com>
2024-04-05 14:13:06 +0900

    style: remove prallax over tiles container background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <div className="w-full h-auto snap-mandatory snap-y">
       <About />
-      <div className="w-full h-auto pb-10 flex flex-col lg:grid grid-cols-2 bg-parallax bg-fixed bg-cover">
+      <div className="w-full h-auto pb-10 flex flex-col lg:grid grid-cols-2">
         <Services />
         <Equipments />
         <Doctors />
