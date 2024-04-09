1fde1f29ed75681ee634d3afc8d9a2e6dd3175e6
Ed Cuevas <edjanncuevas@icloud.com>
2024-04-09 11:29:50 +0900

    feat: implement doctors info component

--- src/app/components/information/DoctorsInfo.tsx
+++ src/app/components/information/DoctorsInfo.tsx
@@ -3,8 +3,14 @@ import React from 'react';
 const DoctorsInfo = () => {
 
   return (
-    <div>
-      
+    <div className='w-full h-auto'>
+      <div className='w-full flex flex-col md:flex-row items-center justify-center md:justify-around p-2'>
+        <div className='w-[100px] h-[150px] lg:w-[250px] lg:h-[350px] bg-dr_Ito bg-cover bg-center'></div>
+        <p>Dr. Ito</p>
+        <p>Info descriptiojasfsdkjbsd blah blah blah</p>
+      </div>
     </div>
   )
 }
+
+export default DoctorsInfo;
--- src/app/pages/doctors/page.tsx
+++ src/app/pages/doctors/page.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "../../../styles/globals.css";
+import DoctorsInfo from "@/app/components/information/DoctorsInfo";
 
 const DoctorsPage: React.FC<DoctorsPageProps> = () => {
   return (
     <div className="page-container">
       <div className="w-full h-full bg-white p-2">
-        <div className="w-full h-[150px] md:h-[250px] lg:h-[500px] bg-doctors_full bg-center bg-cover"/>
+        <div className="w-full h-[150px] md:h-[250px] lg:h-[500px] bg-doctors_full bg-center bg-cover bordero-b-2"/>
           <div className="w-full h-auto mb-4">
-            
+            <DoctorsInfo />
           </div>
       </div>  
     </div>
