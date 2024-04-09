d096cb3654efb1fe66db9487e02f68c906d68bfb
Ed Cuevas <edjanncuevas@icloud.com>
2024-04-09 11:49:09 +0900

    style: stylize doctors page and info

--- src/app/components/information/DoctorsInfo.tsx
+++ src/app/components/information/DoctorsInfo.tsx
@@ -4,10 +4,12 @@ const DoctorsInfo = () => {
 
   return (
     <div className='w-full h-auto'>
-      <div className='w-full flex flex-col md:flex-row items-center justify-center md:justify-around p-2'>
-        <div className='w-[100px] h-[150px] lg:w-[250px] lg:h-[350px] bg-dr_Ito bg-cover bg-center'></div>
-        <p>Dr. Ito</p>
-        <p>Info descriptiojasfsdkjbsd blah blah blah</p>
+      <div className='w-full h-full p-2 md:p-4 lg:p-8'>
+        <div className='w-full flex flex-col md:flex-row items-center justify-center md:justify-around p-2 rounded-lg shadow-xl bg-white hover:scale-105 duration-500'>
+          <div className='w-[100px] h-[150px] md:w-[150px] md:h-[220px] lg:w-[250px] lg:h-[350px] bg-dr_Ito bg-cover bg-center'></div>
+          <p className='p-4'>Dr. Ito</p>
+          <p className='p-4'>Info descriptiojasfsdkjbsd blah blah blah</p>
+        </div>
       </div>
     </div>
   )
--- src/app/pages/doctors/page.tsx
+++ src/app/pages/doctors/page.tsx
@@ -5,7 +5,7 @@ import DoctorsInfo from "@/app/components/information/DoctorsInfo";
 const DoctorsPage: React.FC<DoctorsPageProps> = () => {
   return (
     <div className="page-container">
-      <div className="w-full h-full bg-white p-2">
+      <div className="w-full h-full bg-white/10 p-2">
         <div className="w-full h-[150px] md:h-[250px] lg:h-[500px] bg-doctors_full bg-center bg-cover bordero-b-2"/>
           <div className="w-full h-auto mb-4">
             <DoctorsInfo />
