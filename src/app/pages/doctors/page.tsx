7869d127cffd7fdb4032c6243ea3571f08296e2b
Ed Cuevas <edjanncuevas@icloud.com>
2024-04-09 10:30:40 +0900

    feat: initialize doctors landing page

--- src/app/pages/doctors/page.tsx
+++ src/app/pages/doctors/page.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import "../../../styles/globals.css";
 
 const DoctorsPage: React.FC<DoctorsPageProps> = () => {
-  return <div className="page-container">sdfsdfsdf</div>;
+  return (
+    <div className="page-container">
+      <div className="w-full h-full bg-white p-2">
+        <div className="w-full h-[150px] md:h-[250px] lg:h-[500px] bg-doctors_full bg-center bg-cover"/>
+          <div className="w-full h-auto mb-4">
+            
+          </div>
+      </div>  
+    </div>
+  );
 };
 
 export default DoctorsPage;
