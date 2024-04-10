45ae5815b0d4b94e4d715bc59671bc714a26f71d
EdjannCuevas <edjanncuevas@icloud.com>
2024-04-10 09:13:59 +0900

    style: set background of doctors info to white blur

--- src/app/pages/doctors/page.tsx
+++ src/app/pages/doctors/page.tsx
@@ -5,8 +5,8 @@ import DoctorsInfo from "@/app/components/information/DoctorsInfo";
 const DoctorsPage: React.FC<DoctorsPageProps> = () => {
   return (
     <div className="page-container">
-      <div className="w-full h-full bg-white/10 p-2">
-        <div className="w-full h-[150px] md:h-[250px] lg:h-[500px] bg-doctors_full bg-center bg-cover bordero-b-2"/>
+      <div className="w-full h-full bg-white/20 border-2 border-white p-2">
+        <div className="w-full h-[150px] md:h-[250px] lg:h-[500px] bg-doctors_full bg-center bg-cover"/>
           <div className="w-full h-auto mb-4">
             <DoctorsInfo />
           </div>
