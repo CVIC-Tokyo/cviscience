d4c050fcb0f18cdd128d3f6899ec89ef97e6f836
EdjannCuevas <edjanncuevas@icloud.com>
2024-04-11 15:06:26 +0900

    delete: remove mission component

--- src/app/components/information/Mission.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../../../styles/globals.css";
-
-const Mission = () => {
-  return <div className="w-full max-w-[1700px] h-auto"></div>;
-};
-
-export default Mission;
