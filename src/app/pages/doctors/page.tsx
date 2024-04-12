46dd9374c8753485c0d88fc934e413575786b135
Ed Cuevas <edjanncuevas@icloud.com>
2024-04-12 16:03:17 +0900

    feat: add and implement doctors history carousel component

--- src/app/pages/doctors/page.tsx
+++ src/app/pages/doctors/page.tsx
@@ -5,6 +5,7 @@ import "../../../styles/globals.css";
 import DoctorsInfo from "@/app/components/information/DoctorsInfo";
 import { getLocaleData } from "@/utils/helpers";
 import { useGlobalContext } from "@/context/store";
+import History_Carousel from "@/app/components/information/HistoryCarousel";
 
 const DoctorsPage: React.FC<DoctorsPageProps> = () => {
   const { locale } = useGlobalContext();
@@ -12,14 +13,15 @@ const DoctorsPage: React.FC<DoctorsPageProps> = () => {
 
   return (
     <div className="page-container">
-      <div className="w-full h-full bg-white/20 border-2 border-cvic-red p-2">
-        <div className="w-full h-[150px] md:h-[250px] lg:h-[500px] bg-doctors_full bg-center bg-cover">
-          <div className="w-full h-full flex justify-center items-center text-cvic-red font-bold text-2xl md:text-4xl">
-            {localeData.DOCTORS.TITLE}
-          </div>
+      <div className="w-full h-[150px] md:h-[250px] lg:h-[500px] bg-doctors_full bg-center bg-cover">
+        <div className="w-full h-full flex justify-center items-center text-cvic-red font-bold text-2xl md:text-4xl">
+          {localeData.DOCTORS.TITLE}
         </div>
+      </div>
+      <div className="w-full h-full bg-white/20 p-2">
         <div className="w-full h-auto mb-4">
           <DoctorsInfo />
+          <History_Carousel locale={locale} />
         </div>
       </div>
     </div>
