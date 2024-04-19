e6a1db58bbb9d5baf45c8eff0ef24a4846b3c6ec
Ed Cuevas <edjanncuevas@icloud.com>
2024-04-19 16:37:07 +0900

    style: adjust text sizes for footer component

--- src/app/components/footercomponents/SNS.tsx
+++ src/app/components/footercomponents/SNS.tsx
@@ -4,6 +4,10 @@ import facebook from "../../../../public/logos/facebook.webp";
 import youtube from "../../../../public/logos/youtube.png";
 import gmaps from "../../../../public/logos/gmaps.png";
 
+function opneLinkInNewTab(url: string) {
+  window.open(url, '_blank');
+}
+
 const SNS = () => {
   return (
     <div className="w-full max-w-[500px] h-[40px] flex items-center justify-center">
@@ -13,6 +17,7 @@ const SNS = () => {
         height={45}
         width={45}
         className="sns-logo"
+        onClick={() => opneLinkInNewTab('https://www.facebook.com/CVICIidabashi')}
       />
       <Image
         src={youtube}
@@ -20,6 +25,7 @@ const SNS = () => {
         height={55}
         width={55}
         className="sns-logo"
+        onClick={() => 'https://www.youtube.com/channel/UCiU_fpZA-P_VNvwwn8ESrLw'}
       />
       <Image
         src={gmaps}
@@ -27,6 +33,7 @@ const SNS = () => {
         height={35}
         width={35}
         className="sns-logo"
+        onClick={() => opneLinkInNewTab('https://www.google.com/maps/place/CVIC+Medical+Corporation+%2F+Cardiovascular+Imaging+Clinic+Iidabashi/@35.7047471,139.7410899,16z/data=!3m2!4b1!5s0x60188c4f8b977bc3:0x32c5083c13aa1e03!4m6!3m5!1s0x60188c4f8c8f4919:0x797025f7a7942b1d!8m2!3d35.7047428!4d139.7436648!16s%2Fg%2F1tfd6zfz?entry=ttu')}
       />
     </div>
   );
