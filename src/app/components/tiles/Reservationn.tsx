7d534605b0a29a6114fcb96c8838ab9ba84f1d5b
Ed Cuevas <edjanncuevas@icloud.com>
2024-04-22 19:58:12 +0900

    feat: replace staff page with reservation page

--- src/app/components/tiles/Reservationn.tsx
+++ src/app/components/tiles/Reservationn.tsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 import Reveal from "../Reveal";
 
-const Staff: React.FC<StaffProps> = () => {
+const Reservation: React.FC<ReservationProps> = () => {
   const [zoomOut, setZoomOut] = useState(false);
   const { locale } = useGlobalContext();
   const localeData = getLocaleData(locale);
@@ -62,7 +62,7 @@ const Staff: React.FC<StaffProps> = () => {
           }}
           className="w-full h-full bg-staff bg-cover bg-center"
         ></div>
-        <Link href={"/pages/staff"} className="tile-info-container">
+        <Link href={"/pages/reservation"} className="tile-info-container">
           <div
             className="w-[150px] md:w-auto h-auto flex flex-wrap flex-col items-center md:items-start justify-center"
             style={{
@@ -72,7 +72,7 @@ const Staff: React.FC<StaffProps> = () => {
           >
             <Reveal>
               <p className="md:my-2 text-white text-[10px] md:text-base font-bold h-[30px]">
-                {localeData.STAFF.STAFF}
+                {localeData.RESERVATION.TITLE}
               </p>
             </Reveal>
             <Reveal>
@@ -95,4 +95,4 @@ const Staff: React.FC<StaffProps> = () => {
   );
 };
 
-export default Staff;
+export default Reservation;
