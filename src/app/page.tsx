ec97f715efef08a97bd5530b06100da33b00fb57
Ed Cuevas <edjanncuevas@icloud.com>
2024-04-23 09:50:40 +0900

    feat: show clinic info before tile components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,13 +13,13 @@ export default function Home() {
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center">
       <About />
-      <div className="w-full max-w-[1700px] h-auto mb-10 md:mb-32 flex flex-col lg:grid grid-cols-2 p-2">
+      <ClinicInfo />
+      <div className="w-full max-w-[1700px] h-auto mt-10 md:mt-32 flex flex-col lg:grid grid-cols-2 p-2">
         <Services />
         <Equipments />
         <Doctors />
         <Staff />
       </div>
-      <ClinicInfo />
     </div>
   );
 }
