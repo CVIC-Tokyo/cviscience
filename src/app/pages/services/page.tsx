1838c71f22671ded8312723c3e6ecee361b70ab7
Ed Cuevas <edjanncuevas@icloud.com>
2024-04-23 10:28:33 +0900

    feat: initalize service info

--- src/app/pages/services/page.tsx
+++ src/app/pages/services/page.tsx
@@ -1,10 +1,26 @@
+'use client'
+
 import "../../../styles/globals.css";
 import React from "react";
+import { getLocaleData } from "@/utils/helpers";
+import { useGlobalContext } from "@/context/store";
 
 const ServicesPage: React.FC<ServicesPageProps> = () => {
+  const { locale } = useGlobalContext();
+  const localeData = getLocaleData(locale);
+
   return (
     <div className="page-container">
-      <div className="w-full h-auto border-2 border-cvic-red">test</div>
+      <div className="bg-white/25 flex flex-col items-center justify-center">
+        <div className="w-full">
+          <div className="bg-white rounded-lg p-4">
+            <h2 className="text-xl font-bold mb-4">{localeData.HEART_MRI_SCAN.TITLE}</h2>
+            {localeData.HEART_MRI_SCAN.DESCRIPTION.map((paragraph, index) => (
+              <p key={index} className="mb-4">{paragraph}</p>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
