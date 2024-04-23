832e34fa06db28ead993a34cea0ffa866dc05014
Ed Cuevas <edjanncuevas@icloud.com>
2024-04-23 09:20:09 +0900

    feat: create and stylize access page

---
+++ src/app/pages/access/page.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import React from "react";
+import { useGlobalContext } from "@/context/store";
+import "../../../styles/globals.css";
+import { getLocaleData } from "@/utils/helpers";
+
+const Access = () => {
+  const { locale } = useGlobalContext(); // Assuming useGlobalContext provides access to locale data
+  const localeData = getLocaleData(locale);
+
+  return (
+    <div className="page-container">
+      <div className="w-full bg-white/25 p-4">
+        <div className="p-1 md:p-4 text-xs md:text-base">
+          {/* JR Chuo/Sobu Line */}
+          <div className="access-info flex flex-col p-1 bg-white rounded-lg my-2">
+            <h3 className="font-semibold text-lg text-cvic-red mb-2">{localeData.LOCATION_INFORMATION.JR_CHUO_SOBU_LINE.TITLE}</h3>
+            <ul className="list-disc list-inside flex flex-col items-start p-2">
+              {localeData.LOCATION_INFORMATION.JR_CHUO_SOBU_LINE.DESCRIPTION.map((item, index) => (
+                <li key={index} className="mb-1">{item}</li>
+              ))}
+            </ul>
+          </div>
+
+          {/* Tokyo Metro Lines */}
+          <div className="access-info flex flex-col p-1 bg-white rounded-lg my-2">
+            <h3 className="font-semibold text-lg text-cvic-red mb-2">{localeData.LOCATION_INFORMATION.TOKYO_METRO_LINES.TITLE}</h3>
+            <ul className="list-disc list-inside flex flex-col items-start p-2">
+              {localeData.LOCATION_INFORMATION.TOKYO_METRO_LINES.DESCRIPTION.map((item, index) => (
+                <li key={index} className="mb-1">{item}</li>
+              ))}
+            </ul>
+          </div>
+
+          {/* Taxi */}
+          <div className="access-info flex flex-col p-1 bg-white rounded-lg my-2">
+            <h3 className="font-semibold text-lg text-cvic-red mb-2">{localeData.LOCATION_INFORMATION.TAXI.TITLE}</h3>
+            <p className="mb-2">{localeData.LOCATION_INFORMATION.TAXI.DESCRIPTION}</p>
+          </div>
+
+          {/* Car */}
+          <div className="access-info flex flex-col p-1 bg-white rounded-lg my-2">
+            <h3 className="font-semibold text-lg text-cvic-red mb-2">{localeData.LOCATION_INFORMATION.CAR.TITLE}</h3>
+            <p className="mb-2">{localeData.LOCATION_INFORMATION.CAR.DESCRIPTION}</p>
+          </div>
+          <div className="access-info flex flex-col p-1 bg-white rounded-lg my-2">
+            <h3 className="font-semibold text-lg text-cvic-red mb-2">{localeData.LOCATION_INFORMATION.WHEELCHAIR.TITLE}</h3>
+            <p className="mb-2">{localeData.LOCATION_INFORMATION.WHEELCHAIR.DESCRIPTION}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Access;
