0cdd759fc0b8987df7b791639cee391703141a23
Ed Cuevas <edjanncuevas@icloud.com>
2024-04-24 16:34:08 +0900

    feat: add ramili ai chatbot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
           type="text/javascript"
           src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBIC_MAPS_API_KEY}&loading=async`}
         />
+        <script src="https://chatwith.tools/chatbot/d1548dd1-b93a-452e-b254-effd84def9c2.js" async></script>
       </head>
       <body className="noto-serif w-full h-full text-center">
         <Providers>{children}</Providers>
