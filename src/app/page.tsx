56a8e6efcf002dce9d29ae93e6ae6b24ea5b6fa8
Ed Cuevas <edjanncuevas@icloud.com>
2024-04-28 19:01:45 +0900

    feat: initialize QPAS component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,13 @@ import Doctors from "./components/tiles/Doctors";
 import Services from "./components/tiles/Services";
 import ClinicInfo from "./components/information/ClinicInfo";
 import Staff from "./components/tiles/Reservationn";
+import QualitySection from "./components/information/Quality Section";
 
 export default function Home() {
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center">
       <About />
+      <QualitySection />
       <ClinicInfo />
       <div className="w-full max-w-[1700px] h-auto mt-10 md:mt-32 flex flex-col lg:grid grid-cols-2 p-2">
         <Services />
