794d75432ace57b78530d44b0a96fe57ac699c34
Ed Cuevas <edjanncuevas@icloud.com>
2024-04-29 17:06:54 +0900

    feat: redirect qpas info to clinic info component

--- src/app/components/information/ClinicInfo.tsx
+++ src/app/components/information/ClinicInfo.tsx
@@ -6,6 +6,8 @@ import { useGlobalContext } from "@/context/store";
 import { getLocaleData } from "@/utils/helpers";
 import Reveal from "../Reveal";
 import ArrowIcon from "../DropdownArrow";
+import QualitySection from "./QualitySection";
+import Link from "next/link";
 
 const ClinicInfo: React.FC<ClinicInfoProps> = () => {
   const { locale } = useGlobalContext();
@@ -40,10 +42,10 @@ const ClinicInfo: React.FC<ClinicInfoProps> = () => {
   };
 
   return (
-    <div className="max-w-[1700px] w-full flex items-center justify-center tracking-wide">
+    <div className="max-w-[1300px] w-full flex items-center justify-center tracking-wide">
       <div className="w-full h-auto p-2 flex flex-col items-center justify-center">
         <div className="max-w-[1700px] w-full flex items-center justify-center tracking-wide">
-          <div className="w-full h-auto p-2 flex flex-col items-center justify-center">
+          <div className="w-full h-auto flex flex-col items-center justify-center">
             <Reveal>
               <div className="bg-white p-2 m-2 rounded-lg h-auto mx-auto">
                 <Reveal>
@@ -55,7 +57,7 @@ const ClinicInfo: React.FC<ClinicInfoProps> = () => {
                     <ArrowIcon
                       showDropdown={showIntroduction}
                       handleRotation={handleRotationIntroduction}
-                    />
+                    /> 
                   </h1>
                 </Reveal>
                 {showIntroduction && (
@@ -64,22 +66,79 @@ const ClinicInfo: React.FC<ClinicInfoProps> = () => {
                     <p className="font-bold p-2 md:p-4">
                       {localeData.INTRODUCTION.CONTENT}
                     </p>
-                    <p className="p-2 md:p-4">
-                      {localeData.CVIC_DESCRIPTION.CONTENT}
-                    </p>
-                    <p className="p-2 md:p-4">
-                      {localeData.EARLY_DETECTION.CONTENT}
-                    </p>
-                    <p className="p-2 md:p-4">
-                      {localeData.ADVANCED_IMAGING.CONTENT}
-                    </p>
-                    <p className="p-2 md:p-4">
-                      {localeData.ADDRESSING_NEEDS.CONTENT}
-                    </p>
-                    <p className="p-2 pb-0 md:p-4 md:pb-0 font-bold">
-                      {localeData.CONTINUOUS_IMPROVEMENT.CONTENT}
-                    </p>
-                    <p className="">{localeData.NEW_CVIC_GROUP.CONTENT}</p>
+                    <Reveal>
+                      <div className="w-full p-1 md:p-2">
+                        <div className="border-t-1 md:border-t-2 border-cvic-red/50" />
+                        <h1 className="font-bold text-cvic-red tracking-widest p-1 md:p-2">{localeData.CVIC_DESCRIPTION.TITLE}</h1>
+                        <div className="w-full flex flex-col md:grid grid-cols-3">
+                          <Link href={'/pages/equipments'} className="w-full flex-col flex items-center justify-center">
+                            <div className="bg-equipments w-[300px] h-[150px] bg-center bg-cover rounded-lg shadow-lg hover:scale-105 duration-500 cursor-pointer col-span-1"/>
+                            <div className="text-xs p-2 text-blue-500">{localeData.FACILITIES.TITLES.MRI.VINTAGE_TITAN.TITLE}</div>
+                          </Link>
+                          <p className="p-2 md:p-4 col-span-2">
+                            {localeData.CVIC_DESCRIPTION.CONTENT}
+                          </p>
+                        </div>
+                      </div>
+                    </Reveal>
+                    <Reveal>
+                      <div className="w-full p-1 md:p-2">
+                        <div className="border-t-1 md:border-t-2 border-cvic-red/50" />
+                        <h1 className="font-bold text-cvic-red tracking-widest p-1 md:p-2">{localeData.EARLY_DETECTION.TITLE}</h1>
+                        <div className="w-full flex flex-col-reverse md:grid grid-cols-3">
+                          <div className=" col-span-2">
+                            <p className="p-2 md:p-4">
+                              {localeData.EARLY_DETECTION.CONTENT}
+                            </p>
+                            <p className="p-2 md:p-4">
+                              {localeData.ADVANCED_IMAGING.CONTENT}
+                            </p>
+                          </div>
+                          <Link href={'pages/equipments'} className="w-full flex flex-col items-center justify-center">
+                            <div className="bg-zio w-[300px] h-[150px] bg-center bg-cover rounded-lg shadow-lg hover:scale-105 duration-500 cursor-pointer col-span-1"/>
+                            <div className="text-xs p-2 text-blue-500">{localeData.FACILITIES.TITLES.ZIO.TITLE}</div>
+                          </Link>
+                        </div>
+                      </div>
+                    </Reveal>
+                    <Reveal>
+                      <div className="w-full p-1 md:p-2">
+                        <div className="border-t-1 md:border-t-2 border-cvic-red/50" />
+                        <h1 className="font-bold text-cvic-red tracking-widest p-1 md:p-2">{localeData.ADDRESSING_NEEDS.TITLE}</h1>
+                        <div className="w-full flex flex-col md:grid grid-cols-3">
+                          <Link href={'/pages/reservation'} className="w-full flex flex-col items-center justify-center">
+                            <div className="bg-staff w-[300px] h-[150px] bg-center bg-cover rounded-lg shadow-lg hover:scale-105 duration-500 cursor-pointer col-span-1"/>
+                            <div className="text-xs p-2 text-blue-500">{localeData.RESERVATION.TITLE}</div>
+                          </Link>
+                          <p className="p-2 md:p-4 col-span-2">
+                            {localeData.ADDRESSING_NEEDS.CONTENT}
+                          </p>
+                        </div>
+                      </div>
+                    </Reveal>
+                    <Reveal>
+                      <div className="w-full p-1 md:p-2">
+                        <div className="border-t-1 md:border-t-2 border-cvic-red/50" />
+                        <h1 className="font-bold text-cvic-red tracking-widest p-1 md:p-2">{localeData.CONTINUOUS_IMPROVEMENT.TITLE}</h1>
+                        <div className="w-full flex flex-col-reverse md:grid grid-cols-3">
+                          <div className="col-span-2">
+                            <p className="p-2 md:p-4 md:pb-0 font-bold">
+                              {localeData.CONTINUOUS_IMPROVEMENT.CONTENT}
+                            </p>
+                            <p className="p-2 md:p-4">
+                              {localeData.CVIC_DOCK.CONTENT}
+                            </p>
+                          </div>
+                          <div className="w-full flex items-start justify-center">
+                            <div className="bg-case w-[270px] h-[250px] bg-center bg-cover rounded-lg shadow-lg hover:scale-105 duration-500 cursor-pointer col-span-1"/>
+                          </div>
+                        </div>
+                      </div>
+                    </Reveal>
+                    <div className="border-t-1 md:border-t-2 border-cvic-red/50" />
+                    <Reveal>
+                      <p className="text-[10px] md+text-sm">{localeData.NEW_CVIC_GROUP.CONTENT}</p>
+                    </Reveal>
                   </div>
                   </Reveal>
                 )}
@@ -87,6 +146,7 @@ const ClinicInfo: React.FC<ClinicInfoProps> = () => {
             </Reveal>
           </div>
         </div>
+        <QualitySection />
         <Reveal>
           <div className="bg-white p-2 m-2 rounded-lg h-auto mx-auto">
             <Reveal>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,12 @@ import Doctors from "./components/tiles/Doctors";
 import Services from "./components/tiles/Services";
 import ClinicInfo from "./components/information/ClinicInfo";
 import Staff from "./components/tiles/Reservationn";
-import QualitySection from "./components/information/QualitySection";
 import SanityCards from "./components/SanityCards";
 
 export default function Home() {
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center">
       <About />
-      <QualitySection />
       <ClinicInfo />
       <SanityCards />
       <div className="w-full max-w-[1700px] h-auto mt-10 md:mt-32 flex flex-col lg:grid grid-cols-2 p-2">
