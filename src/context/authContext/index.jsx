4ea04c8327830526af23f74a76808fafb8a6a7dc
Ed Cuevas <edjanncuevas@icloud.com>
2024-04-30 05:40:34 +0900

    feat: set up firebase config

---
+++ src/context/authContext/index.jsx
@@ -0,0 +1,42 @@
+import { onAuthStateChanged } from "firebase/auth";
+import React, { useContext, useEffect, useState } from "react";
+import { auth } from '../../app/firebase/config'
+const AuthContext = React.createContext();
+
+export function useAuth() {
+    return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+    const [currentUser, setCurrentUser] = useState(null);
+    const [userLoggedIn, setUserLoggedIn] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, initilizaUser);
+        return unsubscribe;
+    }, []);
+
+    async function initilizaUser(user) {
+        if (user) {
+            setCurrentUser({ ...user })
+            setUserLoggedIn(true);
+        } else {
+            setCurrentUser(null);
+            setUserLoggedIn(false);
+        }
+        setLoading(false);
+    }
+
+    const value = {
+        currentUser,
+        userLoggedIn,
+        loading,
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    )
+}
